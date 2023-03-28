583fc7a0b952bfd90c701e2ac19e3f731775fed9
tS6wG#!#wABQ632 <erwann-marobin@hotmail.fr>
2023-03-28 15:27:56 +0200

    modif

--- back/tests/pins/routes.py
+++ back/tests/pins/routes.py
@@ -3,14 +3,11 @@ from fastapi.testclient import TestClient
 
 app = FastAPI()
 
-
 @app.get("/pins")
 async def read_main():
     return {"id":1,"title":"Toto au parc", "description": "c'est l\'histoire de toto au parc", "tag":['tag1',"tag2","tag3"]}
 
-
 client = TestClient(app)
-
 
 def test_read_main():
     response = client.get("/")
